95bebfc7572f8b99558660b9119e6a8fe373c4a0
Onur <onurdogruiletisim@gmail.com>
2023-01-02 19:11:34 +0300

    Add project files.

---
+++ Foreach/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Foreach
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int toplam = 0;
+            int[] sinavlar = { 70, 65, 85, 100, 90 };
+
+            foreach (int x in sinavlar)
+            {
+                listBox1.Items.Add(x);
+                toplam = toplam + x;
+            }
+            label1.Text = toplam.ToString();
+        }
+    }
+}
